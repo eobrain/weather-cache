7e8b0835dce97e13a0edd3cf1a77040b198409c0
Eamonn O'Brien-Strain <eobrain@gmail.com>
2024-05-24 20:51:32 -0700

    Implement caching

--- main.go
+++ main.go
@@ -8,20 +8,33 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"time"
+
+	cache "github.com/patrickmn/go-cache"
 )
 
 var apiKey string
+var c *cache.Cache
 
 func init() {
 	apiKey = os.Getenv("OPENWEATHERMAP_API_KEY")
 	if apiKey == "" {
 		log.Fatal("environment variable OPENWEATHERMAP_API_KEY is not set")
 	}
+
+	c = cache.New(24*time.Hour, 1*time.Hour)
 }
 
 func api(latScaled int, lonScaled int) (string, error) {
 	cacheKey := fmt.Sprintf("%d:%d", latScaled, lonScaled)
 	fmt.Printf("cacheKey=%s\n", cacheKey)
+
+	result, found := c.Get(cacheKey)
+	if found {
+		fmt.Print("HIT\n")
+		return result.(string), nil
+	}
+	fmt.Print("MISS\n")
 
 	requestURL := fmt.Sprintf(
 		`https://api.openweathermap.org/data/2.5/forecast?lat=%f&lon=%f&units=metric&appid=%s`,
@@ -36,7 +49,12 @@ func api(latScaled int, lonScaled int) (string, error) {
 	fmt.Printf("client: status code: %d\n", res.StatusCode)
 
 	resBody, err := io.ReadAll(res.Body)
-	return string(resBody), nil
+	if err != nil {
+		return "error reading response body", err
+	}
+	json := string(resBody)
+	c.Set(cacheKey, json, cache.DefaultExpiration)
+	return json, nil
 }
 
 func scale(x float64) int {

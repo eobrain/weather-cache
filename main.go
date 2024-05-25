e0eed616b7067efa729c2fc31932c3215232d1bd
Eamonn O'Brien-Strain <eobrain@gmail.com>
2024-05-24 18:34:07 -0700

    Add client call

--- main.go
+++ main.go
@@ -2,13 +2,34 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 )
 
-func api(lat float64, lon float64) string {
-	return fmt.Sprintf("{\"lat\": %f, \"lon\": %f}", lat, lon)
+var apiKey string
+
+func init() {
+	apiKey = os.Getenv("OPENWEATHERMAP_API_KEY")
+	if apiKey == "" {
+		log.Fatal("environment variable OPENWEATHERMAP_API_KEY is not set")
+	}
+}
+
+func api(lat float64, lon float64) (string, error) {
+	requestURL := fmt.Sprintf(`https://api.openweathermap.org/data/2.5/weather?lat=%f&lon=%f&units=metric&appid=%s`, lat, lon, apiKey)
+	res, err := http.Get(requestURL)
+	if err != nil {
+		return fmt.Sprintf("error making http request: '%s'", requestURL), err
+	}
+
+	fmt.Printf("client: got response!\n")
+	fmt.Printf("client: status code: %d\n", res.StatusCode)
+
+	resBody, err := io.ReadAll(res.Body)
+	return string(resBody), nil
 }
 
 func Api(w http.ResponseWriter, req *http.Request) {
@@ -27,8 +48,14 @@ func Api(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	w.Header().Set("Content-Type", "text/jsom")
-	w.Write([]byte(api(lat, lon)))
+	w.Header().Set("Content-Type", "application/json")
+	result, err := api(lat, lon)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.Write([]byte(result))
 	// fmt.Fprintf(w, "This is an example server.\n")
 	// io.WriteString(w, "This is an example server.\n")
 }
